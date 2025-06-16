cb926099bde33a6e70f38d05606e1d784c2171f6
liz-heart <dlc>
2025-06-16 19:47:11 +0200

    Add correct flake.nix f

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My modular NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.git ];
+        };
+      }
+    ) // {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./nixos/hosts/nixos.nix ];
+        };
+      };
+    };
+}
