a8d14dfcdfad75daba8a0f3b487b9ffa5cef4f42
liz-heart <dlc>
2025-06-16 19:20:11 +0200

    Restructure to modular flake: add flake.nix and split configs

---
+++ nixos/modules/common.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable Flakes and nix-command for everyone
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # System packages for all hosts
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+    # add more tools here
+  ];
+
+  # Set timezone
+  time.timeZone = "Europe/Vienna";
+
+  # Example: default editor
+  programs.vim.defaultEditor = true;
+}
