5118fba6e3f86da1ff7c596ad81db6065487d32a
liz-heart <dlc>
2025-06-30 03:13:02 +0200

    add lock

--- nixos/home/laptop.nix
+++ nixos/home/laptop.nix
@@ -15,97 +15,96 @@ in {
   # Autostart-Skript beim Hyprland-Start einbinden
   home.file.".config/hypr/start.sh".source = ./startup/start.sh;
 
-  # Hyprpaper: Hintergrundbild setzen über Konfigurationsdatei
+  # Hintergrundbild setzen über Hyprpaper (Home-Manager verwaltet Konfig)
   home.file.".config/hypr/hyprpaper.conf".text = ''
     preload = /home/dominik/my_nix/wallpapers/w1.png
     wallpaper = eDP-1,/home/dominik/my_nix/wallpapers/w1.png
   '';
 
-  # Installierte Pakete
+  # Installierte Programme
   home.packages = with pkgs; [
-    # Terminal & Systemtools
-    zsh                    # Shell
-    neofetch               # Systeminfo im Terminal
-    btop                   # Ressourcenmonitor (modernes htop)
-    swww                   # Wallpaper-Übergänge (optional)
-    wl-clipboard           # Clipboard für Wayland
-    clipman                # Clipboard-Manager
-    waybar                 # Statusleiste
-    networkmanagerapplet   # GUI für WLAN
-    polkit_gnome           # PolicyKit für Admin-Rechte
-    swaylock       # Schöner Lockscreen für Hyprland
+    # Shell & Tools
+    zsh
+    neofetch
+    btop
+    swww
+    wl-clipboard
+    clipman
+    waybar
+    networkmanagerapplet
+    polkit_gnome
 
-    # KDE/Qt Anwendungen
-    kdePackages.dolphin    # Dateimanager (Qt6)
+    # KDE-Anwendungen
+    kdePackages.dolphin    # Dateimanager
     kdePackages.kate       # Texteditor
-    kdePackages.okular     # PDF & Dokumentenanzeige
+    kdePackages.okular     # PDF-Betrachter
     kdePackages.spectacle  # Screenshots
-    kdePackages.gwenview   # Bildbetrachter
+    kdePackages.gwenview   # Bilder anzeigen
+
+    # Bildschirm sperren (klassisch, stabil)
+    swaylock
   ];
 
-  # Hyprland-Konfiguration via Home-Manager
+  # Hyprland-Konfiguration
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
-      # Automatisch gestartete Anwendungen
       exec-once = [
         "${pkgs.hyprpaper}/bin/hyprpaper &"
         "${pkgs.waybar}/bin/waybar &"
         "~/.config/hypr/start.sh"
       ];
 
-      # Tastaturlayout
       input = {
         kb_layout = "at";
         kb_variant = "nodeadkeys";
       };
 
-      # Tastenkürzel
       bind = [
-        # ▶ Programme starten
-        "SUPER, Return, exec, ${pkgs.kitty}/bin/kitty"                  # Terminal
-        "SUPER, Q, exec, ${pkgs.wofi}/bin/wofi --show drun"             # App-Launcher
-        "SUPER, W, exec, ${pkgs.kdePackages.dolphin}/bin/dolphin"       # Dateimanager
-        "SUPER, Z, exec, ${pkgs.swaylock}/bin/swaylock --clock --indicator --grace 3 --effect-blur 7x5"  # Bildschirm sperren
-        "SUPER SHIFT, S, exec, ${pkgs.kdePackages.spectacle}/bin/spectacle"  # Screenshot-Tool
+        # Programme starten
+        "SUPER, Return, exec, ${pkgs.kitty}/bin/kitty"
+        "SUPER, Q, exec, ${pkgs.wofi}/bin/wofi --show drun"
+        "SUPER, W, exec, ${pkgs.kdePackages.dolphin}/bin/dolphin"
+        "SUPER, Z, exec, ${pkgs.swaylock}/bin/swaylock -f -c 000000"
+        "SUPER SHIFT, S, exec, ${pkgs.kdePackages.spectacle}/bin/spectacle"
 
-        # ▶ Fenster- und Workspace-Steuerung
-        "SUPER, R, killactive"               # Aktives Fenster schließen
-        "SUPER, F, togglefloating"           # Floating-Modus an/aus
-        "SUPER, Space, fullscreen"           # Vollbild an/aus
+        # Fenstersteuerung
+        "SUPER, R, killactive"
+        "SUPER, F, togglefloating"
+        "SUPER, Space, fullscreen"
 
-        # ▶ Workspaces wechseln
+        # Workspaces wechseln
         "SUPER, 1, workspace, 1"
         "SUPER, 2, workspace, 2"
         "SUPER, 3, workspace, 3"
         "SUPER, 4, workspace, 4"
         "SUPER, 5, workspace, 5"
 
-        # ▶ Fenster in andere Workspaces verschieben
+        # Fenster in andere Workspaces verschieben
         "SUPER SHIFT, 1, movetoworkspace, 1"
         "SUPER SHIFT, 2, movetoworkspace, 2"
         "SUPER SHIFT, 3, movetoworkspace, 3"
         "SUPER SHIFT, 4, movetoworkspace, 4"
         "SUPER SHIFT, 5, movetoworkspace, 5"
 
-        # ▶ Fokus verschieben (Vim-Stil)
+        # Fokus verschieben (Vim-Stil)
         "SUPER, H, movefocus, l"
         "SUPER, L, movefocus, r"
         "SUPER, K, movefocus, u"
         "SUPER, J, movefocus, d"
 
-        # ▶ Fenster verschieben (Vim-Stil)
+        # Fenster verschieben
         "SUPER SHIFT, H, movewindow, l"
         "SUPER SHIFT, L, movewindow, r"
         "SUPER SHIFT, K, movewindow, u"
         "SUPER SHIFT, J, movewindow, d"
       ];
 
-      # ▶ Maus-Interaktion mit Fenstern
+      # Fenster bewegen / skalieren mit Maus
       bindm = [
-        "SUPER, mouse:272, movewindow"   # Mit Super + Linksklick Fenster ziehen
-        "SUPER, mouse:273, resizewindow" # Mit Super + Rechtsklick Fenstergröße ändern
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
       ];
     };
   };
